5540e0d22ed1a075af3950dc0a9831fbe2c9bf04
gareth <gareth@gareth-P410.com>
2022-12-18 21:51:09 +1100

    add src

---
+++ src/adventOfCode2022.f90
@@ -0,0 +1,10 @@
+module adventOfCode2022
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, adventOfCode2022!"
+  end subroutine say_hello
+end module adventOfCode2022
